fbef7fe45c579ad60453fe1b10f1c32e11b1f26a
Kyrylo <kirillholomozyk@gmail.com>
2022-10-14 13:48:06 +0200

    practice1

---
+++ practice1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the seed number: ");
+        int seed = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("How many numbers do you want to generate?");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        Random rnd = new Random(seed);
+        for (int ctr = 0; ctr <= count; ctr++)
+        {
+            Console.Write("{0,3}   ", rnd.Next(-10, 11));
+        }
+
+    }
+}
+
---
+++ practice1.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+
+class RandomCryptoNumbers
+{
+    public static void Main()
+    {
+        int c = 1;
+        Console.WriteLine("How many numbers do you want to generate?");
+        int count = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i <= count; i++)
+        {
+            var rnd = GenerateRandomNumber();
+            Console.WriteLine(Convert.ToBase64String(rnd));
+            c++;
+
+        }
+    }
+    public static byte[] GenerateRandomNumber()
+    {
+        using (var randomNumberGenerator =
+               new RNGCryptoServiceProvider())
+        {
+            int bit = 32;
+            var randomNumber = new byte[bit];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+
+}
